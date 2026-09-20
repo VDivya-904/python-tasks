29348c8eef325bf346557330cadd2a2faca7039d
VDivya-904 <vdivya22062007@gmail.com>
2026-09-20 07:35:33 +0530

    Add duplicate element finder in list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 20, 40, 50, 30]
+duplicates = []
+for number in numbers:
+    if numbers.count(number) > 1 and number not in duplicates:
+        duplicates.append(number)
+print("Duplicate elements are:", duplicates)
